7a467e888885e32a57b6104587678ce2ffdc4063
Andrej <krtolinat@gmail.com>
2025-06-03 17:04:04 +0200

    Added state for handling field selection

--- src/components/react-flow-graph.tsx
+++ src/components/react-flow-graph.tsx
@@ -14,14 +14,14 @@ import {
   DrawerHeader,
 } from '@heroui/react'
 import type { BlueprintForm, BlueprintNode } from '../types/blueprint-graph'
-import { Database } from 'lucide-react'
-
-type FormField = { nodeId: string; fieldKey: string }
+import { Database, X } from 'lucide-react'
+import type { DrawerFormField, DrawerFormMapping } from '../types/drawer-form'
 
 const ReactFlowGraph = () => {
   const [selectedNode, setSelectedNode] = useState<BlueprintNode>()
-  const [selectedField, setSelectedField] = useState<FormField>()
-  const [selectedMapping, setSelectedMapping] = useState<FormField>()
+  const [selectedField, setSelectedField] = useState<DrawerFormField>()
+  const [selectedMapping, setSelectedMapping] = useState<DrawerFormField>()
+  const [mappings, setMappings] = useState<DrawerFormMapping[]>([])
 
   const { data, isLoading } = useReadBlueprintGraph({
     tenantId: 'MOCK',
@@ -101,12 +101,23 @@ const ReactFlowGraph = () => {
     setSelectedMapping(undefined)
   }, [])
 
-  const handleFieldSelect = useCallback((field: FormField) => {
+  const handleFieldSelect = useCallback((field: DrawerFormField) => {
     setSelectedField(field)
   }, [])
 
-  const handleMappingSelect = useCallback((mapping: FormField) => {
+  const handleMappingSelect = useCallback((mapping: DrawerFormField) => {
     setSelectedMapping(mapping)
+  }, [])
+
+  const handleAddMapping = useCallback(() => {
+    if (!selectedField || !selectedMapping) return
+    setMappings((prev) => [...prev, { field: selectedField, target: selectedMapping }])
+    setSelectedField(undefined)
+    setSelectedMapping(undefined)
+  }, [selectedField, selectedMapping])
+
+  const handleRemoveMapping = useCallback(({ fieldKey, nodeId }: DrawerFormField) => {
+    setMappings((prev) => prev.filter(({ field }) => !(field.fieldKey === fieldKey && field.nodeId === nodeId)))
   }, [])
 
   if (isLoading) return <Loader />
@@ -171,18 +182,42 @@ const ReactFlowGraph = () => {
                   </Accordion>
                 ) : (
                   <ul>
-                    {Object.keys(formMap[selectedNode.data.component_id].field_schema.properties).map((key) => (
-                      <li key={key} className="flex items-center justify-between py-1">
-                        <Button
-                          variant="flat"
-                          className="flex items-center gap-2 w-full justify-start"
-                          startContent={<Database className="h-5 w-5 text-gray-500" />}
-                          onPress={() => handleFieldSelect({ nodeId: selectedNode.id, fieldKey: key })}
-                        >
-                          {key}
-                        </Button>
-                      </li>
-                    ))}
+                    {Object.keys(formMap[selectedNode.data.component_id].field_schema.properties).map((key) => {
+                      const mapping = mappings.find(
+                        ({ field: { fieldKey, nodeId } }) => fieldKey === key && nodeId === selectedNode.id
+                      )
+
+                      return (
+                        <li key={key} className="flex items-center justify-between py-1">
+                          {mapping ? (
+                            <div className="flex items-center gap-2 justify-between bg-zinc-700 py-1 px-4 w-full rounded-xl">
+                              <p className="text-sm">
+                                {mapping.field.fieldKey}: {nodeMap[mapping.target.nodeId].data.name}.
+                                {mapping.target.fieldKey}
+                              </p>
+                              <Button
+                                size="sm"
+                                variant="light"
+                                color="danger"
+                                isIconOnly
+                                onPress={() => handleRemoveMapping(mapping.field)}
+                              >
+                                <X />
+                              </Button>
+                            </div>
+                          ) : (
+                            <Button
+                              variant="flat"
+                              className="flex items-center gap-2 w-full justify-start"
+                              startContent={<Database className="h-5 w-5 text-gray-500" />}
+                              onPress={() => handleFieldSelect({ nodeId: selectedNode.id, fieldKey: key })}
+                            >
+                              {key}
+                            </Button>
+                          )}
+                        </li>
+                      )
+                    })}
                   </ul>
                 )}
               </DrawerBody>
@@ -197,7 +232,7 @@ const ReactFlowGraph = () => {
                   </Button>
                 )}
                 {selectedField ? (
-                  <Button color="primary" isDisabled={!selectedMapping}>
+                  <Button color="primary" isDisabled={!selectedMapping} onPress={handleAddMapping}>
                     Select
                   </Button>
                 ) : null}
---
+++ src/types/drawer-form.ts
@@ -0,0 +1,2 @@
+export type DrawerFormField = { nodeId: string; fieldKey: string }
+export type DrawerFormMapping = { field: DrawerFormField; target: DrawerFormField }
